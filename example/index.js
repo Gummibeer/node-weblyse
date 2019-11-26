49f9a63efd248d6c161c53e1f7fcdcd320a5e3c8
Tom Witkowski <dev.gummibeer@gmail.com>
2019-11-26 18:01:31 +0100

    add some more details to example

--- example/index.js
+++ example/index.js
@@ -2,9 +2,20 @@ const watchtower = require('../src/index');
 
 process.chdir(__dirname);
 
-watchtower([
+watchtower(
+    [
     'https://github.com',
-], './report.json')
+    ],
+    './report.json' // if you don't need a file just omit this argument
+)
+    .then(data => {
+        // do whatever you want with the data Object
+        // * assertions in your CI
+        // * render a template
+        // * post to monitoring service
+        // ...
+    })
     .finally(() => {
+        // that's needed to end your node process
        process.exit(0);
     });
