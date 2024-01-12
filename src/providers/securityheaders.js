4fc952c1ae5c034cf5b1f6cc3c7f5fdd63a7bf68
Bob Schockweiler <info@webfashion.in>
2024-01-12 19:56:29 +0100

    Add additional security header check

--- src/providers/securityheaders.js
+++ src/providers/securityheaders.js
@@ -9,6 +9,7 @@ module.exports = function ({ followRedirects, hide }) {
                         'x-frame-options': ['SAMEORIGIN', 'DENY'].includes(response.headers['x-frame-options']),
                         'x-xss-protection': response.headers['x-xss-protection'] === '1; mode=block',
                         'content-security-policy': response.headers['content-security-policy'] !== undefined,
+                        'x-content-type-options': response.headers['x-content-type-options'] === 'nosniff',
                     };
                     console.log('securityheaders: ' + url);
                 });
